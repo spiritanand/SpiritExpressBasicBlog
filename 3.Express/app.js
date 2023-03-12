b6dd37be07bb73de626de7ed0a5003619eb1e46f
Suryansh Anand <suryansh.anand42@gmail.com>
2023-03-12 23:10:13 +0530

    Added Express server

--- 3.Express/app.js
+++ 3.Express/app.js
@@ -1,43 +1,25 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+const blogRoutes = require("./routes/blogRoutes");
 
 // setup express app
 const app = express();
 
+// connect to MongoDB
+const dbURI = `mongodb+srv://spirtinnovates:PKGEReKWKn6ypbDM@learnnode.ftd43i2.mongodb.net/NetNinjaBlog?retryWrites=true&w=majority`;
+mongoose.connect(dbURI)
+		.then(res => {
+		  console.log("connected to db");
+		  // we should listen for requests only once the connection has been made to
+		  // the DB
+		  app.listen(3000);
+		})
+		.catch(err => console.log(err));
+
 // register view engine
 app.set("view engine", "ejs");
 // app.set("views", "<folder-name-where-views-are-stored>")
-
-// listen for requests
-app.listen(3000);
-
-
-// this is how we return (normal) html pages in using express
-
-// routing
-
-// app.get("/", (req, res) => {
-// res.send("<p>Inside express app</p>");
-//   res.sendFile("./views/index.html", {root: __dirname});
-// });
-
-// app.get("/about", (req, res) => {
-//   // res.send("<h1>About Page</h1>");
-//   res.sendFile("./views/about.html", {root: __dirname});
-// });
-
-// redirects
-// app.get("/about-us", (req, res) => {
-//   res.redirect("/about");
-// });
-
-// 404 page
-// this needs to be at the bottom of our code. otherwise if express reaches this then
-// this will be the response sent to the browser. At the bottom it works like a catch-all
-// app.use((req, res) => {
-//   res.status(404)
-// 	 .sendFile("./views/404.html", {root: __dirname});
-// });
 
 // using 3rd party middleware for logging
 app.use(morgan("dev"));
@@ -45,16 +27,31 @@ app.use(morgan("dev"));
 // middleware for accessing static files
 app.use(express.static("public"));
 
+// middleware to get form data
+app.use(express.urlencoded({extended: true}));
+
 // now we will see how to serve a whole view
 app.get("/", (req, res) => {
-  res.render("index", {title: "Home"});
+  res.redirect("/blogs");
 });
+
 app.get("/about", (req, res, next) => {
   res.render("about", {title: "About"});
 });
-app.get("/blogs/create", (req, res) => {
-  res.render("create", {title: "Create"});
-});
+
+// app.delete("/blogs/:id", (req, res) => {
+//   const id = req.params.id;
+//   Blog.findByIdAndDelete(id)
+// 	  .then((result) => {
+// 		console.log(result);
+// 		res.json({redirect:"/"})
+// 	  })
+// 	  .catch(err => console.log(err));
+//   ;
+// });
+
+app.use("/blogs", blogRoutes);
+
 app.use((req, res) => {
   res.status(404)
 	 .render("404", {title: "404"});
